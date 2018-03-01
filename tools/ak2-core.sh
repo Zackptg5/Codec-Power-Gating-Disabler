a39cb20fea9529b659d33853b51813b6596474df
Zackptg5 <zackptg5@comcast.net>
2018-03-01 10:25:45 -0500

    Update busybox and ak2

--- tools/ak2-core.sh
+++ tools/ak2-core.sh
@@ -10,7 +10,8 @@ ACTION=<ACTION>;
 chmod -R 755 $bin;
 mkdir -p $ramdisk $split_img;
 
-OUTFD=/proc/self/fd/$1;
+FD=$1;
+OUTFD=/proc/self/fd/$FD;
 
 # ui_print <text>
 ui_print() { echo -e "ui_print $1\nui_print" > $OUTFD; }
@@ -103,7 +104,7 @@ unpack_ramdisk() {
   mkdir -p $ramdisk;                 
   chmod 755 $ramdisk;
   cd $ramdisk;
-  $unpackcmd -dc $split_img/boot.img-ramdisk.cpio.$compext | cpio -i -d;
+  $unpackcmd -dc $split_img/boot.img-ramdisk.cpio.$compext | EXTRACT_UNSAFE_SYMLINKS=1 cpio -i -d;
   if [ $? != 0 -o -z "$(ls $ramdisk)" ]; then
     ui_print " "; ui_print "Unpacking ramdisk failed. Aborting..."; exit 1;
   fi;
@@ -216,8 +217,8 @@ flash_boot() {
   done;
   if [ ! "$dtb" -a -f *-dtb ]; then
     dtb=`ls *-dtb`;
-    dtb="--dt $split_img/$dtb";
     rpm="$split_img/$dtb,rpm";                          
+    dtb="--dt $split_img/$dtb";
   fi;
   cd /tmp/anykernel;
   if [ -f "$bin/mkmtkhdr" ]; then
@@ -385,7 +386,7 @@ insert_line() {
       after) offset=1;;
     esac;
     line=$((`grep -n "$4" $1 | head -n1 | cut -d: -f1` + offset));
-    if [ "$(wc -l $1 | cut -d\  -f1)" -lt "$line" ]; then
+    if [ -f $1 ] && [ "$(wc -l $1 | cut -d\  -f1)" -lt "$line" ]; then
       echo "$5" >> $1;
     else
       sed -i "${line}s;^;${5}\n;" $1;
